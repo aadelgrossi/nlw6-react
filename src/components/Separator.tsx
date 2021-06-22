33973dd226045b74caf6622cc9ad7ade425a6088
Andre Grossi <iamandregrossi@gmail.com>
2021-06-21 23:50:31 -0300

    Create Separator component

---
+++ src/components/Separator.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react'
+
+import { Flex } from '@chakra-ui/react'
+
+interface SeparatorProps {
+  children: ReactNode
+}
+
+export const Separator = ({ children }: SeparatorProps): JSX.Element => {
+  return (
+    <Flex
+      _before={{
+        content: "''",
+        flex: 1,
+        height: '1px',
+        background: 'lightgray',
+        marginRight: '1rem'
+      }}
+      _after={{
+        content: "''",
+        flex: 1,
+        height: '1px',
+        background: 'lightgray',
+        marginLeft: '1rem'
+      }}
+      align="center"
+      className="divider"
+      fontSize="14px"
+      my="32px"
+    >
+      {children}
+    </Flex>
+  )
+}
