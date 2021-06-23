ebe60e41bd6d55a277c9d500ce99d963f83259e2
Andre Grossi <iamandregrossi@gmail.com>
2021-06-23 20:01:21 -0300

    Turn primary left splash into a separate component

---
+++ src/components/SplashBar.tsx
@@ -0,0 +1,24 @@
+import { Flex, Image, Heading, Text } from '@chakra-ui/react'
+
+export const SplashBar = (): JSX.Element => {
+  return (
+    <Flex
+      justify="center"
+      direction="column"
+      as="aside"
+      flex={7}
+      bg="primary"
+      color="white"
+      py="120px"
+      px="80px"
+    >
+      <Image maxW="320px" src="/assets/illustration.svg"></Image>
+      <Heading lineHeight="42px" mt={2} as="h1">
+        Crie salas de Q&amp;A ao vivo{' '}
+      </Heading>
+      <Text fontSize="1.5em" mt={4}>
+        Tire as dúvidas da sua audiência em tempo real
+      </Text>
+    </Flex>
+  )
+}
