b2467edbcf570e47162ef5bf0ff8f22602a1a25f
Andre Grossi <iamandregrossi@gmail.com>
2021-06-23 22:37:16 -0300

    Add joinRoom method in useRoom

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import { useCallback } from 'react'
 
-import { Flex, Image, Input, Icon } from '@chakra-ui/react'
+import { Flex, Image, Input, Icon, useToast } from '@chakra-ui/react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
 import { CgLogIn } from 'react-icons/cg'
 import { ImGoogle } from 'react-icons/im'
 
-import { useAuth } from '~/auth/useAuth'
+import { useAuth } from '~/auth'
 import { Separator, Button, SplashBar } from '~/components'
+import { useRoom } from '~/rooms'
+
+type JoinRoomFormData = { id: string }
 
 const IndexPage = (): JSX.Element => {
   const { login, user } = useAuth()
   const router = useRouter()
+  const { register, handleSubmit } = useForm<JoinRoomFormData>()
+  const { joinRoom } = useRoom()
+  const toast = useToast()
 
   const handleCreateRoom = useCallback(async () => {
     if (!user) {
@@ -20,6 +27,28 @@ const IndexPage = (): JSX.Element => {
 
     router.push('/rooms/new')
   }, [login, router, user])
+
+  const onSubmit = useCallback(
+    ({ id }: JoinRoomFormData) => {
+      try {
+        joinRoom(id)
+        router.push(`/rooms/${id}`)
+        toast({
+          title: 'Seja bem-vindo',
+          status: 'success',
+          position: 'top-right'
+        })
+      } catch (e) {
+        toast({
+          title: 'Sala não encontrada',
+          description: 'Favor verifique o código da sala',
+          status: 'error',
+          position: 'top-right'
+        })
+      }
+    },
+    [joinRoom, toast, router]
+  )
 
   return (
     <>
@@ -54,7 +83,11 @@ const IndexPage = (): JSX.Element => {
               Crie sua sala com o Google
             </Button>
             <Separator>ou entre em uma sala</Separator>
-            <Flex as="form" direction="column">
+            <Flex
+              as="form"
+              direction="column"
+              onSubmit={handleSubmit(onSubmit)}
+            >
               <Input
                 w="full"
                 h="50px"
@@ -63,6 +96,7 @@ const IndexPage = (): JSX.Element => {
                 bg="white"
                 border="1px"
                 borderColor="gray.400"
+                {...register('id')}
                 placeholder="Digite o código da sala"
               />
               <Button
--- src/rooms/useRoom.ts
+++ src/rooms/useRoom.ts
@@ -1,15 +1,14 @@
 import { useCallback } from 'react'
 
-import { database, firebase } from '~/auth/firebase'
+import { database, firebase } from '~/auth'
 
 import { CreateRoomData } from './types'
-
-const roomRef = database.ref('rooms')
 
 interface RoomHookData {
   createRoom: (
     data: CreateRoomData
   ) => Promise<firebase.database.ThenableReference>
+  joinRoom: (id: string) => Promise<void>
 }
 
 const useRoom = (): RoomHookData => {
@@ -17,12 +16,22 @@ const useRoom = (): RoomHookData => {
     async (
       data: CreateRoomData
     ): Promise<firebase.database.ThenableReference> => {
+      const roomRef = database.ref('rooms')
+
       return roomRef.push({ ...data, createdAt: new Date() })
     },
     []
   )
 
-  return { createRoom }
+  const joinRoom = useCallback(async (id: string): Promise<void> => {
+    const roomRef = await database.ref(`rooms/${id}`).get()
+
+    if (!roomRef.exists()) {
+      throw new Error()
+    }
+  }, [])
+
+  return { createRoom, joinRoom }
 }
 
 export { useRoom }
