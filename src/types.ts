b7c17651d3584b0e8e74eab790332d2ea80b5fb0
Andre Grossi <iamandregrossi@gmail.com>
2021-06-25 00:41:19 -0300

    Create types for Author, Question and Room

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export interface Author {
+  name: string
+  avatar: string
+}
+
+export interface Question {
+  id: string
+  author: Author
+  content: string
+  isAnswered: boolean
+  isHighlighted: boolean
+}
+
+export interface Room {
+  id: string
+  name: string
+  authorId: string
+  initialQuestions: Question[]
+}
