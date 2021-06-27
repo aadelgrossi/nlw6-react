8a224ee3c0587e7d56d2383b7baaed41ff8e38aa
Andre Grossi <iamandregrossi@gmail.com>
2021-06-27 19:11:58 -0300

    Add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,9 +21,33 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto:wght@400;500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&family=Roboto:wght@400;500&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#835afd" />
+          <meta name="apple-mobile-web-app-title" content="LetMeAsk" />
+          <meta name="application-name" content="LetMeAsk" />
+          <meta name="msapplication-TileColor" content="#835afd" />
+          <meta name="theme-color" content="#f8f8f8" />
         </Head>
         <body>
           <Main />
